759c3d016ea20c7892dcd3ec1f490c6fc60f76da
Yeansovanvathana <61142466+Yeansovanvathana@users.noreply.github.com>
2023-10-12 10:40:27 +0700

    update UI

--- src/background.js
+++
@@ -1,9 +0,0 @@
-// Add a listener to handle messages from the popup
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === "takeScreenShot") {
-    const screenshotUrl = request.payload.croppedImage;
-
-    // Send a response back to the popup (optional)
-    sendResponse({ message: "Response from background script" });
-  }
-});
